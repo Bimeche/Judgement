cd07f3a4b3d9ae7387525b058104d85a63739bb0
Rylith <ogaoni@gmail.com>
2016-10-08 04:44:59 -0400

    PrefabsUpdate

--- Assets/Scripts/Map/MapGenerator.cs
+++ Assets/Scripts/Map/MapGenerator.cs
@@ -1,5 +1,5 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
 public class MapGenerator : MonoBehaviour {
 
@@ -9,14 +9,22 @@ public class MapGenerator : MonoBehaviour {
 
     void MapSetup()
     {
+        int num;
+        bool[] usedtiles = new bool[9] { false, false, false, false, false, false, false, false, false };
         MapHolder = new GameObject("Map").transform;
 
         for (int x = 0; x < size; x++)
         {
             for (int y = 0; y < size; y++)
             {
-                GameObject toInstantiate = tiles[Random.Range(0, tiles.Length)];
-                GameObject instance = UnityEngine.Object.Instantiate(toInstantiate, new Vector3(x*10.8f,y*10.8f,0f), Quaternion.identity) as GameObject;
+                num = Random.Range(0, tiles.Length);
+                while (usedtiles [num] == true)
+                {
+                    num = Random.Range(0, tiles.Length);
+                }
+                usedtiles[num] = true;
+                GameObject toInstantiate = tiles[num];
+                GameObject instance = UnityEngine.Object.Instantiate(toInstantiate, new Vector3(x*1080f,y*1080f,0f), Quaternion.identity) as GameObject;
                 instance.transform.SetParent(MapHolder);
             }
         }
