a415665dc9095dd38b496119c29cda3770229230
Rylith <ogaoni@gmail.com>
2016-10-08 03:00:44 -0400

    MapGeneration + ProtoMainMenu
    Add of a random map + Prototype of a main menu

---
+++ Assets/Scripts/Map/MapGenerator.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapGenerator : MonoBehaviour {
+
+    private int size = 3;
+    private Transform MapHolder;
+    public GameObject[] tiles;
+
+    void MapSetup()
+    {
+        MapHolder = new GameObject("Map").transform;
+
+        for (int x = 0; x < size; x++)
+        {
+            for (int y = 0; y < size; y++)
+            {
+                GameObject toInstantiate = tiles[Random.Range(0, tiles.Length)];
+                GameObject instance = UnityEngine.Object.Instantiate(toInstantiate, new Vector3(x*10.8f,y*10.8f,0f), Quaternion.identity) as GameObject;
+                instance.transform.SetParent(MapHolder);
+            }
+        }
+    }
+
+    void Awake()
+    {
+        MapSetup();
+    }
+
+}
